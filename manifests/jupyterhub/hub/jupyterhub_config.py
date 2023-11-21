0c8ba3ab2226a94ac6a65e46efca8f5bb96aa956
Narek Engibaryan <narek030601@yandex.ru>
2023-11-21 03:13:07 +0300

    JupyterHub Oauth

--- manifests/jupyterhub/hub/jupyterhub_config.py
+++ manifests/jupyterhub/hub/jupyterhub_config.py
@@ -1,5 +1,5 @@
 import os
-from firstuseauthenticator import FirstUseAuthenticator
+from oauthenticator.generic import GenericOAuthenticator
 
 c.JupyterHub.spawner_class = "dockerspawner.DockerSpawner"
 c.DockerSpawner.image = os.environ["DOCKER_JUPYTER_IMAGE"]
@@ -13,8 +13,17 @@ c.JupyterHub.services = [
         "command": "cull_idle_servers.py --timeout=3600".split(),
     },
 ]
-c.JupyterHub.authenticator_class = "firstuseauthenticator.FirstUseAuthenticator"
-c.Authenticator.admin_users = {"admin"}
+c.JupyterHub.authenticator_class = GenericOAuthenticator
+c.GenericOAuthenticator.client_id = os.environ['OAUTH_CLIENT_ID']
+c.GenericOAuthenticator.client_secret = os.environ['OAUTH_CLIENT_SECRET']
+c.GenericOAuthenticator.authorize_url = os.environ['OAUTH_AUTHORIZE_URL']
+c.GenericOAuthenticator.token_url = os.environ['OAUTH_TOKEN_URL']
+c.GenericOAuthenticator.userdata_url = os.environ['OAUTH_USERDATA_URL']
+c.GenericOAuthenticator.scope = ["openid", "email", "profile"]
+c.GenericOAuthenticator.username_claim = "preferred_username"
+c.GenericOAuthenticator.claim_groups_key = "groups"
+
+c.Authenticator.admin_users = {"narek01", "zhiyanov"}
 c.DockerSpawner.notebook_dir = "/home/jovyan"
 c.Spawner.default_url = "/lab" # use JupyterLab (instead of Notebook) by default
 c.Spawner.cpu_limit = 1
